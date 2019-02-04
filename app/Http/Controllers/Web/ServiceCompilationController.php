ea00321c417bc037d8ec059ae3ed7265f3420982
Avokald <baldykov_a@yahoo.com>
2019-02-04 12:59:30 +0600

    Make initial classes of compilations

---
+++ app/Http/Controllers/Web/ServiceCompilationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\ServiceCompilation;
+use Illuminate\Http\Request;
+
+class ServiceCompilationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ServiceCompilation  $serviceCompilation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ServiceCompilation $serviceCompilation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ServiceCompilation  $serviceCompilation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ServiceCompilation $serviceCompilation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ServiceCompilation  $serviceCompilation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ServiceCompilation $serviceCompilation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ServiceCompilation  $serviceCompilation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ServiceCompilation $serviceCompilation)
+    {
+        //
+    }
+}
