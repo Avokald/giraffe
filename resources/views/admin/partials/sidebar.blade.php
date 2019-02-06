136d1c39ccca344d3039014f4f6d3fbe6236b49b
Avokald <baldykov_a@yahoo.com>
2019-02-06 13:06:11 +0600

    Add admin sidebar links

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -4,10 +4,46 @@
     <div class="sidebar-content">
         <div class="side-content">
             <ul class="nav-main">
-                <a>Name</a>
-                <a>Dashboard</a>
-                <a>Configuration</a>
-                <a>Pages</a>
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu">Сервисы</a>
+                    <ul>
+                        <li><a href="{{ route('admin.services.index') }}">Все</a></li>
+                        <li><a href="{{ route('admin.services.create') }}">Создать</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu">Подборки</a>
+                    <ul>
+                        <li><a href="{{ route('admin.compilations.index') }}">Все</a></li>
+                        <li><a href="{{ route('admin.compilations.create') }}">Создать</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu">Категории</a>
+                    <ul>
+                        <li><a href="{{ route('admin.categories.index') }}">Все</a></li>
+                        <li><a href="{{ route('admin.categories.create') }}">Создать</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu">Блог</a>
+                    <ul>
+                        <li><a href="{{ route('admin.blog.index') }}">Все</a></li>
+                        <li><a href="{{ route('admin.blog.create') }}">Создать</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu">Страницы</a>
+                    <ul>
+                        <li><a href="{{ route('admin.pages.index') }}">Все</a></li>
+                        <li><a href="{{ route('admin.pages.create') }}">Создать</a></li>
+                    </ul>
+                </li>
+
                 <a>Logout</a>
             </ul>
 
