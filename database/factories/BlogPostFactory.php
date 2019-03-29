73e0275827432f8ad978095b841243e81f5526de
Avokald <=>
2019-03-29 13:29:14 +0600

    Fill test blog post and service compilation with values from design

--- database/factories/BlogPostFactory.php
+++ database/factories/BlogPostFactory.php
@@ -14,13 +14,31 @@ $factory->define(App\BlogPost::class, function (Faker $faker) {
 $factory->state(App\BlogPost::class, 'test', function(Faker $faker) {
     return [
         'title' => 'Title for test blog post',
-        'content' => '<p><i>dsfdsfd</i>sfco<strong>ntent2</strong></p><ol>
-            <li>fsdfsdf<a href="http://www.example.com/example">http://www.example.com/example</a> \
-            </li><li>sdfsdfsd</li></ol><h2>da 1231231231</h2><ul><li>sdfsdfdsfsd</li><li>fdsfdsf</li>
-            </ul><figure class="table"><table><thead><tr><th>1</th><th>2</th></tr></thead><tbody><tr>
-            <td>3</td><td>4</td></tr></tbody></table></figure><blockquote><p>dsdsadsdadwddscasdfassad</p>
-            </blockquote><h3>hgh</h3><p>asdasd</p><h4>asdsd</h4><p>sadasd</p>',
-        'excerpt' => '123 excerpt for test blog post',
+        'content' => '
+        <p>
+        Nunc placerat mi id nisi interdum mollis. Praesent pharetra, justo ut scelerisque the mattis, leo quam aliquet congue. incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in henderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat sunt in culpa qui officia sunt in culpa qui officia.
+        </p>
+        <blockquote>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indus try’s standard.</blockquote>
+        <h2>We just know now gonna make</h2>
+        <p>Lorem Ipsum is simply dummy text of the and typesetting industry. Lorem Ipsum is has been the industry’s stasn ndard dummy text ever since the 1500s, when an unknown printer took a galley of it to make. Lorem Ipsum is the simply dummy text of the printing.</p>
+        <ol>
+            <li>List one is awesome. Adding list item is very easily.</li>
+            <li>List one is awesome. Adding list item is very easily.</li>
+            <li>List one is awesome. Adding list item is very easily.</li>
+        </ol>
+        <p>
+        Lorem Ipsum is simply dummy text of the and typesetting industry. Lorem Ipsum is has been the industry’s stasn ndard dummy text ever since the 1500s, when an unknown printer took a galley of it to make. Lorem Ipsum is the simply dummy text of the printing.
+        </p>
+        <ul>
+            <li>Listing things in a list</li>
+            <li>Pay close attention to number</li>
+            <li>What happens next will shock you</li>
+        </ul>
+        <h2>We just know now gonna make</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the and typesetting industry. Lorem Ipsum is has been the industry’s stasn ndard dummy text ever since the 1500s, when an unknown printer took a galley of it to make. Lorem Ipsum is the simply dummy text of the printing.
+        </p>
+        ',
         'author_id' => 1,
     ];
 });
